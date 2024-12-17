93ef757f4510adc88b608cd66fb086c4bd04992c
Sahil Rajput <sahilrajput03@gmail.com>
2024-12-17 17:27:49 +0530

    Update.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
+# Readme
+
+Deployed at _[https://browser-for-terminal.vercel.app](https://browser-for-terminal.vercel.app)_
+
 Refer a.js file for initial testing with shell commands with nodejs.
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,33 @@
 <script lang="ts">
 	import '../app.css';
+	import { dev } from '$app/environment';
+
 	let { children } = $props();
 </script>
 
 <div class="p-4">
 	<!-- App Heading -->
 	<div class="mb-3">
-		<a href="/">
-			<h1 class="mb-2 text-2xl font-bold hover:text-blue-600">Browser For Terminal</h1>
-		</a>
+		<div>
+			<a href="/">
+				<h1 class="mb-2 text-2xl font-bold hover:text-blue-600">Browser For Terminal</h1>
+			</a>
+			{#if dev}
+				<div class="text-xs italic">
+					Deployed at:
+					<a
+						target="_blank"
+						href="https://browser-for-terminal.vercel.app"
+						class="text-blue-600 underline"
+					>
+						browser-for-terminal.vercel.app
+					</a>
+				</div>
+			{/if}
+		</div>
+
+		<br />
+
 		<i class="text-xs text-green-500">
 			With great power comes great responsibility!
 			<span class="text-xs text-gray-500"> (Tip: Use Enter to run command in non-live mode) </span>
