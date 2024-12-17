91af42ffbf043b426018153bac2a3786a4b2fb2d
Sahil Rajput <sahilrajput03@gmail.com>
2024-12-17 17:43:48 +0530

    Update.

--- src/lib/Command.svelte
+++ src/lib/Command.svelte
@@ -59,7 +59,7 @@
 			class="border-1 mt-1 max-h-[200px] overflow-y-auto rounded-md border border-[gray] px-[10px] py-[10px]"
 		>
 			<!-- When `error` is returned by `spawnSync` in server I show my custom error `command` not found {error.path}` because we get vague error from spawn as ENOENT which basically means that command does not exists or file, foler you're trying to access does not exists. -->
-			<pre style="font-size: 0.75rem">{stdout}{stderr}{error
+			<pre class="text-[0.75rem]">{stdout}{stderr}{error
 					? `command not found: ${error.path}`
 					: ''}</pre>
 		</div>
@@ -67,11 +67,15 @@
 </div>
 
 {#snippet componentDebugger()}
-	<label class="select-none text-xs">
+	<label class="select-none text-xs text-[gray]">
 		<input class="ml-3 h-2 w-2" type="checkbox" bind:checked={showDebugger} /> Debugger
 	</label>
 
 	{#if showDebugger}
-		<pre><div>{JSON.stringify({ command, stdout, stderr, error: error ?? '' }, null, 2)}</div></pre>
+		<pre class="mt-1 text-xs text-[gray]"><div>{JSON.stringify(
+					{ command, stdout, stderr, error: error ?? '' },
+					null,
+					2
+				)}</div></pre>
 	{/if}
 {/snippet}
