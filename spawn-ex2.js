9f37c7ce30d5bcff465ba4f76208e5d8e9bac10a
Sahil Rajput <sahilrajput03@gmail.com>
2024-12-19 13:22:11 +0530

    Update.

--- spawn-ex2.js
+++ spawn-ex2.js
@@ -2,6 +2,8 @@ import { spawn, spawnSync } from 'child_process';
 
 const command = `echo 1; sleep 1; echo 2; sleep 1; echo "Bye"`; // Replace with your command
 
+// 🚀🚀 STREAMING IN FRONTEND: https://chatgpt.com/c/6763c4b6-e060-8007-a3c7-da04bb8726b6
+
 // *** MRE Streaming Example***
 const child = spawn('sh', ['-c', command]);
 
