6927990c2577ecf8aec8bed50b64702b8e3a0649
Sahil Rajput <sahilrajput03@gmail.com>
2024-12-19 13:04:23 +0530

    Update.

--- a.js
+++ a.js
@@ -1,7 +1,27 @@
 import { spawnSync } from 'child_process';
 
 // docs - https://nodejs.org/api/child_process.html#child_processspawncommand-args-options
+
+// Command with options
 const { stderr, stdout, error } = spawnSync('ls', ['-al', './src']);
+
+// Workingn with environment variables
+// const { stderr, stdout, error } = spawnSync('sh', ['-c', 'echo $a1'], { // works well
+//     env: {
+//         a1: 10
+//     }
+// });
+
+// Output all environment variables:
+// const { stderr, stdout, error } = spawnSync('env', {
+//     env: {
+//         ...process.env,
+//         a1: 10
+//     }
+// });
+
+
+// logs
 console.log("🚀 ~ stdout:", stdout?.toString());
 console.log("🚀 ~ stderr:", stderr?.toString());
-console.log("🚀 ~ error:", error?.name, error?.message); // bad command
+console.log("🚀 ~ error:", error?.name, error?.message);
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import Command from '$lib/Command.svelte';
+	// Refer file: a.js in this project to view some examples
 </script>
 
 <Command initial={{ command: 'pwd', live: true }} />
+<i>What is not possible? I can not use same terminal session.</i>
